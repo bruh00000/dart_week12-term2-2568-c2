83ec11d42a150b7ae63a4686c4761ce3b1a73746
SuperBussee <lookpee0911@gmail.com>
2026-01-09 10:53:02 +0700

    update constant structor finish2

--- lib/dog.dart
+++ lib/dog.dart
@@ -5,7 +5,9 @@ class Dog{
   final int? dogId;
 
   //2.constant constructor
-  Dog({this.name, this.age, this.dogId}){
+  Dog({this.name, this.age, this.dogId});
+
+  void dogInfo(){
     print("Dog name is : $name");
     print("Dog age is : $age");
     print("Dog ID is : $dogId");
@@ -15,5 +17,5 @@ class Dog{
 void callDog(){
   //3.instance/object
   final Dog dog1 = Dog(name: "Buddy", age: 2, dogId: 101);
-  
+  dog1.dogInfo();
 }
