17df30ebde62cef213f19ff5826e8b421f382b43
SuperBussee <lookpee0911@gmail.com>
2026-01-09 10:46:31 +0700

    update constant structor finish

--- bin/dart_week12.dart
+++ bin/dart_week12.dart
@@ -1,8 +1,10 @@
-import 'package:dart_week12/cat.dart';
+//import 'package:dart_week12/cat.dart';
+import 'package:dart_week12/dog.dart';
 //import 'package:dart_week12/staff.dart';
 
 
 void main() {
   //staff();
-  callCat();
+  //callCat();
+  callDog();
 }
---
+++ lib/dog.dart
@@ -0,0 +1,19 @@
+class Dog{
+  //1.properties 
+  final String? name;
+  final int? age;
+  final int? dogId;
+
+  //2.constant constructor
+  Dog({this.name, this.age, this.dogId}){
+    print("Dog name is : $name");
+    print("Dog age is : $age");
+    print("Dog ID is : $dogId");
+  }
+}
+
+void callDog(){
+  //3.instance/object
+  final Dog dog1 = Dog(name: "Buddy", age: 2, dogId: 101);
+  
+}
